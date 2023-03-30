8b3cac5fd3f15eea79e774d27cf246512b3aa3bc
EyUp Student <eyupstudent@EyUps-MacBook-Air.local>
2023-03-30 09:36:22 +0100

    added unit tests for validation

--- validation.js
+++ validation.js
@@ -3,6 +3,10 @@ export const isValidCountry = (value) => {
     return value !== 'not selected'
 }
 
+export const isRatingSelected = (value) => {
+    return Number(value) > 0;
+}
+
 export const isDateSelected = (value) => {
     return value !== ""
 }
@@ -10,11 +14,9 @@ export const isDateSelected = (value) => {
 export const isDatePast  = (value) => {
     const dateEntered = Math.ceil(new Date(value).getTime()/(1000 * 60 * 60 * 24))
     const dateToday = Math.ceil(new Date().getTime()/(1000 * 60 * 60 * 24))
-    console.log(dateEntered)
-    console.log(dateToday)
     return dateToday > dateEntered
 }
 
-export const isRatingSelected = (value) => {
-    return Number(value) > 0;
+export const isValidDate = (value) => {
+    return isDateSelected(value) && isDatePast(value)
 }
